7e05d3993697dfc32349faa9f4d8d9643d9aef1b
777marc@gmail.com <777marc@gmail.com>
2021-12-31 16:16:30 -0500

    adding question method

--- build/core/game.d.ts
+++ build/core/game.d.ts
@@ -1,1 +1,9 @@
 export declare const generateGamecode: () => string;
+export declare const getQuestions: () => {
+    category: string;
+    type: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+}[];
--- build/core/game.js
+++ build/core/game.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateGamecode = void 0;
+exports.getQuestions = exports.generateGamecode = void 0;
 const generateGamecode = () => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const length = 9;
@@ -13,3 +13,53 @@ const generateGamecode = () => {
     return randomStr;
 };
 exports.generateGamecode = generateGamecode;
+const getQuestions = () => {
+    const questions = [
+        {
+            category: "Entertainment: Film",
+            type: "multiple",
+            difficulty: "easy",
+            question: "Which of the following was not one of &quot;The Magnificent Seven&quot;?",
+            correct_answer: "Clint Eastwood",
+            incorrect_answers: ["Steve McQueen", "Charles Bronson", "Robert Vaughn"],
+        },
+        {
+            category: "Entertainment: Film",
+            type: "multiple",
+            difficulty: "easy",
+            question: "Which of the following movies was not based on a novel by Stephen King? ",
+            correct_answer: "The Thing",
+            incorrect_answers: ["Carrie", "Misery", "The Green Mile"],
+        },
+        {
+            category: "Entertainment: Film",
+            type: "multiple",
+            difficulty: "medium",
+            question: "What is the name of the dog that played Toto in the 1939 film &quot;The Wizard of Oz&quot;?",
+            correct_answer: "Terry",
+            incorrect_answers: ["Tommy", "Teddy", "Toto"],
+        },
+        {
+            category: "Entertainment: Film",
+            type: "multiple",
+            difficulty: "medium",
+            question: "What is the name of the Artificial Intelligence system in the 1983 film, &quot;WarGames&quot;?",
+            correct_answer: "War Operation Plan Response",
+            incorrect_answers: [
+                "Self Evolving Thought Helix",
+                "Master Control Program",
+                "West Campus Analog Computer",
+            ],
+        },
+        {
+            category: "Entertainment: Film",
+            type: "multiple",
+            difficulty: "medium",
+            question: "Who played Sgt. Gordon Elias in &#039;Platoon&#039; (1986)?",
+            correct_answer: "Willem Dafoe",
+            incorrect_answers: ["Charlie Sheen", "Matt Damon", "Johnny Depp"],
+        },
+    ];
+    return questions;
+};
+exports.getQuestions = getQuestions;
